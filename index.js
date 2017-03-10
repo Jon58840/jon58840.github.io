25e392a00bcb54707e78d99dfae24a6b73735fda
Jon58840 <jon58840@hotmail.com>
2017-03-09 23:24:47 -0700

    stuff

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ http.listen( port, function () {
     console.log('listening on port', port);
 });
 
-app.use(express.static(__dirname + '/public'));
-
 // listen to 'chat' messages
 io.on('connection', function(socket){
     socket.on('chat', function(msg){
