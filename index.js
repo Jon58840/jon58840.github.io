4b8518daad3b4b2891166343140a62ab057220c7
Jon58840 <jon58840@hotmail.com>
2017-03-09 23:12:51 -0700

    reset index.js

--- index.js
+++ index.js
@@ -4,16 +4,11 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
-/*
-app.get('/', function(req, res) {
-	res.sendFile('/index.html');
-});
-*/
-http.listen(port, function() {
+http.listen( port, function () {
     console.log('listening on port', port);
 });
 
+app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
 io.on('connection', function(socket){
