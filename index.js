f159744015d10356b9640b8b9aa1fa5555e35acd
Jon58840 <jon58840@hotmail.com>
2017-03-09 22:38:17 -0700

    fml

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public' + '/index.html');
+	res.sendFile('/index.html');
 });
 
 http.listen(port, function() {
