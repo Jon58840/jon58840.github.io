d008def63e809677400c2ba993d30ce20cccea08
Jon58840 <jon58840@hotmail.com>
2017-03-09 22:24:16 -0700

    Diecctory changes and stuff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile('index.html');
 });
 
 http.listen(port, function() {
